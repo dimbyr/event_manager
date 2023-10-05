ed80ee67d592c1f2be644d75e3c65fa9c0158172
Dimby Rabearivony <drabearivony@gmail.com>
2023-10-05 23:38:39 +0200

    Amazing: only one line clean_zipcode function

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,28 @@
 require 'csv'
-puts 'Event Manager Initialized!'
 
-contents = CSV.open('event_attendees.csv', headers: true)
-contents.each do |row|
-  name = row[2]
-  puts name
+def clean_zipcode(zipcode)
+  # if zipcode.nil?
+  #   '00000'
+  # elsif zipcode.length < 5
+  #   zipcode.rjust(5, '0')
+  #   # zipcode = "0#{zipcode}" until zipcode.length == 5
+  # elsif zipcode.length > 5
+  #   zipcode[0, 5]
+  # else 
+  #   zipcode
+  # end
+  zipcode.to_s.rjust(5, '0')[0,5]
 end
 
+puts 'Event Manager Initialized!'
+
+contents = CSV.open(
+  'event_attendees.csv', 
+  headers: true,
+  header_converters: :symbol
+)
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  puts "#{name} : #{zipcode}"
+end
